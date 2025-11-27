9882630254c04f925901aa7ebade1ad209206ba9
raginiSahu11 <ragini.sahu@icloud.com>
2025-11-27 13:47:28 +0530

    [Ragini] add : added utility.js

---
+++ utility.js
@@ -0,0 +1,10 @@
+let x = 16.75;
+
+let summary = `
+Rounded Value: ${Math.round(x)}
+Square Root: ${Math.sqrt(x)}
+x³ Value: ${Math.pow(x, 3)}
+Random (10-50): ${Math.floor(Math.random() * 41) + 10}
+`;
+
+console.log(summary);
