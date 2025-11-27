f48742c592e89dfff04c8438abf5348d52af7fe1
raginiSahu11 <ragini.sahu@icloud.com>
2025-11-27 13:41:03 +0530

    [Ragini] add : added Quiz.js

---
+++ Quiz.js
@@ -0,0 +1,19 @@
+// Q9 - Random Math Quiz Generator
+
+let num1 = Math.floor(Math.random() * 20) + 1;
+let num2 = Math.floor(Math.random() * 20) + 1;
+let operators = ['+', '-', '*', '/'];
+let op = operators[Math.floor(Math.random() * operators.length)];
+
+let answer;
+
+// Use switch to calculate based on operator
+switch(op){
+    case '+': answer = num1 + num2; break;
+    case '-': answer = num1 - num2; break;
+    case '*': answer = num1 * num2; break;
+    case '/': answer = (num1 / num2).toFixed(2); break;
+}
+
+console.log(`Question: ${num1} ${op} ${num2}`);
+console.log("Correct Answer:", answer);
